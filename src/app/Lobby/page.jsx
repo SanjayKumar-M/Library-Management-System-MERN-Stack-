1fd0f98f0c715cf8b61b329d56f20e5909c92310
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 00:17:41 +0530

    ui fixed

--- src/app/Lobby/page.jsx
+++ src/app/Lobby/page.jsx
@@ -8,19 +8,15 @@ const Lobby = () => {
 
             </div>
 
-            <div className='flex items-center justify-center flex-col gap-4'>
+            <div className='flex items-center justify-center flex-col gap-7 mt-52 '>
 
 
-                <input className='border-black' />
-                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-2xl'> Join Meet</button>
-                <p>OR</p>
+                <input className='border-black bg-slate-200 p-4 rounded-2xl text-l' placeholder='Enter id'/>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-2xl font-bold text-center text-xl'> Join Meet</button>
+                <p className='font-extrabold text-center text-xl font-mono' >OR</p>
 
-                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-xl'>New Meet</button>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-xl font-bold text-center text-xl'>New Meet</button>
             </div>
-
-            
-
-
 
         </div>
     )
