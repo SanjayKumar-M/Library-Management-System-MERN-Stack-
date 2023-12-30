6bb67e5e8483adfc14c16dddd8165da219a1ee36
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-30 23:42:00 +0530

    update

--- src/context/Socketclient.js
+++ src/context/Socketclient.js
@@ -5,26 +5,33 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext);
-    return socket;
+  const socket = useContext(SocketContext);
+  return socket;
 };
 
 export const Socketclient = (props) => {
-    const { children } = props;
-    const [socket, setSocket] = useState(null);
+  const { children } = props;
+  const [socket, setSocket] = useState(null);
 
-    useEffect(() => {
-        const connection = io();
-        console.log("socket connection", connection);
-        setSocket(connection);
-    }, []);
+  useEffect(() => {
+    const connection = io();
+    console.log("socket connection", connection);
+    setSocket(connection);
+  }, []);
 
+  useEffect(() => {
     socket?.on('connect_error', async (err) => {
-        console.log("Error establishing socket", err);
-        await fetch('/api/socket');
+      console.log("Error establishing socket", err);
+      await fetch('/api/socket');
     });
 
-    return (
-        <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
-    );
+    return () => {
+      // Disconnect the socket when the component unmounts
+      socket?.disconnect();
+    };
+  }, [socket]);
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
 };
