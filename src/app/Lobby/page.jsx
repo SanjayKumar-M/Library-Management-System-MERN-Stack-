09237ac2075b1ac2d4f63d10ac8459404b2196f3
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 00:26:52 +0530

    uuid setup

--- src/app/Lobby/page.jsx
+++ src/app/Lobby/page.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const Lobby = () => {
+    const router = useRouter()
+    const [roomId, setRoomId] = useState('')
+  
+    const createAndJoin = () => {
+      const roomId = uuidv4()
+      router.push(`/${roomId}`)
+    }
+  
+    const joinRoom = () => {
+      if (roomId) router.push(`/${roomId}`)
+      else {
+        alert("Please provide a valid room id")
+      }
+    }
     return (
         <div >
             <div className='flex items-center justify-center p-11 bg-slate-500'>
@@ -11,11 +28,11 @@ const Lobby = () => {
             <div className='flex items-center justify-center flex-col gap-7 mt-52 '>
 
 
-                <input className='border-black bg-slate-200 p-4 rounded-2xl text-l' placeholder='Enter id'/>
-                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-2xl font-bold text-center text-xl'> Join Meet</button>
+                <input className='border-black bg-slate-200 p-4 rounded-2xl text-l' placeholder='Enter id' value={roomId} onChange={(e) => setRoomId(e?.target?.value)}/>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-2xl font-bold text-center text-xl' onClick={joinRoom}> Join Meet</button>
                 <p className='font-extrabold text-center text-xl font-mono' >OR</p>
 
-                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-xl font-bold text-center text-xl'>New Meet</button>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-xl font-bold text-center text-xl' onClick={createAndJoin}>New Meet</button>
             </div>
 
         </div>
