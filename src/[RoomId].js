c92d668f2aa5ae8e2c8075f149f7510b978f80fc
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-30 23:19:33 +0530

    roomid

--- src/[RoomId].js
+++ src/[RoomId].js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSocket } from './context/Socketclient'
+import usePeer from './app/hooks/usePeer';
 
 const Room = () => {
+    const socket = useSocket();
+    const {peer, myId} = usePeer();
   return (
     <div>Room</div>
   )
