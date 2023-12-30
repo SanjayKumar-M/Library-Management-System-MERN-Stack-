9371ac6b0ff370d97c165b667baeef0861c9c5e2
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 00:04:50 +0530

    style update

--- src/app/Lobby/page.jsx
+++ src/app/Lobby/page.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 
 const Lobby = () => {
     return (
-        <>
-            <div className='flex items-center justify-center p-11 bg-slate-500'> 
+        <div >
+            <div className='flex items-center justify-center p-11 bg-slate-500'>
                 <p className='font-extrabold text-center text-5xl font-mono'>Cosmic Collab</p>
-                
+
             </div>
 
-            <div className='flex items-center justify-center flex-col mt-80'>
+            <div className='flex items-center justify-center flex-col gap-4'>
 
 
                 <input className='border-black' />
-                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full'> Join Meet</button>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-2xl'> Join Meet</button>
                 <p>OR</p>
 
-                <button className='bg-black text-white p-3 pl-3 rounded-full'>Create New Meet</button>
+                <button className='bg-black text-white p-3 pl-11 pr-11 rounded-full shadow-xl'>New Meet</button>
             </div>
-        </>
+
+            
+
+
+
+        </div>
     )
 }
 
