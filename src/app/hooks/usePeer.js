be12a2b7657221be6f32aa543aa7fb00cec34072
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-30 19:47:26 +0530

    peerjs

---
+++ src/app/hooks/usePeer.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+import { useSocket } from "@/context/Socketclient";
+
+const usePeer = () =>{
+    const [peer, setPeer] = useState(null);
+    const [myId, setmyId] = useState('')
+    useEffect(()=>{
+        
+    },[])
+}
+
+export default usePeer;
