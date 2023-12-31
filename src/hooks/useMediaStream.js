03b54ee04d5222a42359fbd31de23f0d1ea4c6ce
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 23:48:17 +0530

    try catch implementation

--- src/hooks/useMediaStream.js
+++ src/hooks/useMediaStream.js
@@ -4,12 +4,17 @@ const useMediaStream = () =>{
     const [state,setState] = useState(null);
     useEffect(()=>{
         (async function initStream(){
+            try{
             const stream = await navigator.mediaDevices.getUserMedia({
                 audio:true,
                 video:true
             })
             console.log("setting your stream");
             setState(stream)
+        }
+        catch(e){
+            console.log(e)
+        }
         })
     },[])
 }
