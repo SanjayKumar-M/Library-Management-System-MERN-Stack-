9be15eb85edc4c1c2dc5f8ce2bc286ce1cc291a9
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 23:46:52 +0530

    mediadevices stream setup

--- src/hooks/useMediaStream.js
+++ src/hooks/useMediaStream.js
@@ -2,6 +2,14 @@ import { useState ,useEffect} from "react";
 const useMediaStream = () =>{
 
     const [state,setState] = useState(null);
+    useEffect(()=>{
+        (async function initStream(){
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio:true,
+                video:true
+            })
+        })
+    })
 }
 
 export default useMediaStream;
