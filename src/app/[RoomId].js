fe3fbf4be3734939cb09f89895671374df5ec4c3
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-31 23:50:23 +0530

    Room id setup

--- src/app/[RoomId].js
+++ src/app/[RoomId].js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useSocket } from '../context/Socketclient'
 import usePeer from '../hooks/usePeer';
+import useMediaStream from '@/hooks/useMediaStream';
 
 const Room = () => {
     const socket = useSocket();
     const {peer, myId} = usePeer();
+    const {stream} = useMediaStream();
   return (
     <div>Room</div>
   )
